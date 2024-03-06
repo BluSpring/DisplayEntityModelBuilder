1016150710dbddcc3e6baae7437e941f0a003ab0
Naz <bluspringindohq@gmail.com>
2024-03-07 07:43:35 +0800

    oops

--- src/main/kotlin/xyz/bluspring/displayentitymodelbuilder/DisplayEntityModelBuilder.kt
+++ src/main/kotlin/xyz/bluspring/displayentitymodelbuilder/DisplayEntityModelBuilder.kt
@@ -125,7 +125,7 @@ class DisplayEntityModelBuilder : ModInitializer {
                         try {
                             val json = JsonParser.parseReader(resource.openAsReader()).asJsonObject
 
-                            val format = ModelFormat.deserialize(ResourceLocation(id.namespace, id.path.removeSuffix(".model.json").removeSuffix(".json")), json)
+                            val format = ModelFormat.deserialize(ResourceLocation(id.namespace, id.path.removeSuffix(".model.json").removeSuffix(".json").removePrefix("models/")), json)
                             Registry.register(AtrophyRegistry.MODEL_FORMAT, format.id, format)
                             logger.info("Registered model $id as ${format.id}")
                         } catch (e: Exception) {
